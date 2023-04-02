5ee10d55f6ae19c4e9c89d3c927b45906eea70c9
Justin <jgjudd40@gmail.com>
2023-04-01 22:12:22 -0400

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 
+import {Route, Routes} from 'react-router-dom'
+
 import Header from './components/Header'
+import LandingPage from './pages/LandingPage'
 
 function App() {
   return (
     <>
       <Header />
       <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
       </div>
     </>
   )
