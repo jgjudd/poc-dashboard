5d9b250c26654953775721ec026a5c941ea314ea
Justin <jgjudd40@gmail.com>
2023-04-21 02:21:58 -0400

    new router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
 import './App.css'
-
-import {Route, Routes} from 'react-router-dom'
 import Header from './components/MUI/Header'
 import LandingPage from './pages/LandingPage'
 import ExcelTool from './pages/ExcelTool'
-import RouterDemo from './pages/RouterDemo'
 
-function App() {
+import { 
+  Route, 
+  RouterProvider, 
+  createBrowserRouter, 
+  createRoutesFromElements,
+  Outlet
+} from "react-router-dom"
+
+const Layout = () => {
   return (
     <>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/excel" element={<ExcelTool />} />
-          <Route path="/router" element={<RouterDemo />} />
-        </Routes>
-      </div>
+      <Header />
+      <Outlet />
     </>
   )
 }
 
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<Layout />}>
+    <Route index element={<LandingPage />} />
+    <Route path="/excel" element={<ExcelTool />} />
+    <Route path="/router" element={<div><div>Router Layout</div><Outlet /></div>}>
+      <Route index element={<div>Router Page</div>} loader={()=>{return null}} />
+      <Route path='/router/user' element={<div>User Content</div>} loader={()=>{return null}} />
+      <Route path='/router/admin' element={<div>Admin Content</div>} loader={()=>{return null}} />
+    </Route>
+  </Route>
+))
+
+const App = () => {
+  return <RouterProvider router={router} />
+}
 export default App
