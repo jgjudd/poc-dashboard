b956c67ecabb2b7f41b08480c9e6fcd841c19501
Justin <jgjudd40@gmail.com>
2023-05-01 00:01:24 -0400

    bored

--- src/pages/CalculatorTool.jsx
+++ src/pages/CalculatorTool.jsx
@@ -43,17 +43,46 @@ const CalculatorTool = () => {
   return ( 
     <>
       <h1 style={{ textAlign: 'center' }}>Calculator Tool</h1>
-      <div id='calculator-container'>
-        <input type='text' value={displayValue} onChange={e => setDisplayValue(e.target.value)} />
-        <button onClick={() => handleOperation('+', displayValue)}>Add</button>
-        <button onClick={() => handleOperation('-', displayValue)}>Subtract</button>
-        <button onClick={() => handleOperation('*', displayValue)}>Multiply</button>
-        <button onClick={() => handleOperation('/', displayValue)}>Divide</button>
-        <button onClick={calculateTotal}>Equals</button>
-
+      <div id='calculator-wrapper' style={wrapper}>
+        <input type='text' value={displayValue} style={inputField} onChange={e => setDisplayValue(e.target.value)} />
+        <div id='calculator-container' style={calculatorStyles}>
+          <button onClick={() => handleOperation('+', displayValue)}>Add</button>
+          <button onClick={() => handleOperation('-', displayValue)}>Subtract</button>
+          <button onClick={() => handleOperation('*', displayValue)}>Multiply</button>
+          <button onClick={() => handleOperation('/', displayValue)}>Divide</button>
+          <button onClick={() => handleOperation('sin', displayValue)}>sin</button>
+          <button onClick={() => handleOperation('cos', displayValue)}>cos</button>
+          <button onClick={() => handleOperation('tan', displayValue)}>tan</button>
+          <button onClick={() => handleOperation('log', displayValue)}>log</button>
+          <button onClick={() => handleOperation('sqrd', displayValue)}>Squared</button>
+          <button onClick={() => handleOperation('sqrt', displayValue)}>Square Root</button>
+          <button onClick={calculateTotal}>Equals</button>
+          <button onClick={() => handleOperation('clear', displayValue)}>Clear</button>
+          <button onClick={() => handleOperation('allClear', displayValue)}>All Clear</button>
+        </div>
       </div>
     </>
   )
 }
 
 export default CalculatorTool
+
+const wrapper = {
+  width: '50%',
+  margin: 'auto auto',
+}
+const calculatorStyles = {
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr 1fr',
+  gridGap: '10px',
+  backgroundColor: 'lightgrey',
+  padding: '10px',
+    // flexWrap: 'wrap',
+
+  // justifyContent: 'center',
+  // alignItems: 'center',
+}
+
+const inputField = {
+  width: '100%',
+}
